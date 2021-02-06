3826947df9b076ee4e8d55581908a57736adbaa8
244981745 <37541600+244981745@users.noreply.github.com>
2021-02-06 18:26:55 +0800

    TEST ASM 0
    light

---
+++ Test/T0.asm
@@ -0,0 +1,19 @@
+；点灯程序
+
+.org $8000
+lda	#$ff	;将65c22port A，B设置为输出
+sta	#$7f02	;IO0的Port B
+lda	#$ff	;
+sta	#$7f03	;IO0的Port A
+lda	#$00	;IO0的Port A，Port B 全输出低电平
+sta	#$7f00	;向IO0的Port B写数据
+lda	#$00	;
+sta	#$7f01	;向IO0的Port A写数据
+jmp	#$8000	;跳转到eeprom起始地址
+
+$$-$	db 00	;中间全部填充00
+
+.org $fffa
+dw	$8000
+dw	$8000
+dw	$8000
