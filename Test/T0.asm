3cf0743e76caaa5d0f798fbffc801723f43c24d8
244981745 <37541600+244981745@users.noreply.github.com>
2021-02-07 11:12:44 +0800

    Add files via upload

--- Test/T0.asm
+++ Test/T0.asm
@@ -2,14 +2,14 @@
 
 .org $8000
 lda	#$ff	;将65c22port A，B设置为输出
-sta	#$7f02	;IO0的Port B
+sta	$7f02	;IO0的Port B
 lda	#$ff	;
-sta	#$7f03	;IO0的Port A
+sta	$7f03	;IO0的Port A
 lda	#$00	;IO0的Port A，Port B 全输出低电平
-sta	#$7f00	;向IO0的Port B写数据
+sta	$7f00	;向IO0的Port B写数据
 lda	#$00	;
-sta	#$7f01	;向IO0的Port A写数据
-jmp	#$8000	;跳转到eeprom起始地址
+sta	$7f01	;向IO0的Port A写数据
+jmp	$8000	;跳转到eeprom起始地址
 
 times $7ffa-($-$$)	db 00	;中间全部填充00
 
